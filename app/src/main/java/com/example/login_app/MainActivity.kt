3c513368d238b55baab21d4179804e6c63c78d0a
emidiaz00 <emildsn00@gmail.com>
2022-10-05 18:27:30 -0300

    agrego funcion navegar a otra actividad

--- app/src/main/java/com/example/login_app/MainActivity.kt
+++ app/src/main/java/com/example/login_app/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.login_app
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_hello)
 
+        val buttonLoginHome = findViewById<Button>(R.id.btn_login)
+
+        buttonLoginHome.setOnClickListener {
+            val intent = Intent(this, HelloActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
